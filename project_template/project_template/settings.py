a6ccbe0b75ad43049cb4f8dac19be52193aca746
Bhaskar <bhaskar@cowhite.com>
2016-10-02 23:55:12 +0530

    Setup Postgresql.

--- project_template/project_template/settings.py
+++ project_template/project_template/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'project_template.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'cowhite_codechecker',
+        'NAME': 'cowhite_codechecker',
+        'PASSWORD': 'cowhite_codechecker_pwd',
     }
 }
 
